ee1597a00952d9db8fccc40c4e1d8fd224d35905
Arbel Amram <arbelamram@gmail.com>
2024-07-10 16:46:44 +0300

    REMOVE nav avatar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -29,11 +29,6 @@ function Nav() {
                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1200px-Netflix_2015_logo.svg.png"
                 alt="Netflix Logo"
             />
-            <img
-                className="nav_avatar"
-                src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
-                alt="Netflix user Logo"
-            />
         </div>
     )
 }
