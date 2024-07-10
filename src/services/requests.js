9cdd5f02d1cba2db85d7a2325d2e5b5e47dd01b2
Arbel Amram <arbelamram@gmail.com>
2024-07-10 16:46:00 +0300

    extract information to avoid repetition

--- src/services/requests.js
+++ src/services/requests.js
@@ -1,14 +1,15 @@
 const API_KEY = process.env.REACT_APP_API_KEY
+const base_url = `/discover/movie?api_key=${API_KEY}&with_genres`
 
 const requests = {
     fetchTranding: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
     fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
     fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
-    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
-    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
-    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
-    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
-    fetchDocumentryMovies: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+    fetchActionMovies: `${base_url}=28`,
+    fetchComedyMovies: `${base_url}=35`,
+    fetchHorrorMovies: `${base_url}=27`,
+    fetchRomanceMovies: `${base_url}=10749`,
+    fetchDocumentryMovies: `${base_url}=99`,
 }
 
 export default requests
