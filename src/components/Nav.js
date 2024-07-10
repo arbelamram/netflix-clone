da9a8810ed5e51c101b2ddab690e83ad5d8b229f
Arbel Amram <arbelamram@gmail.com>
2024-07-10 17:27:59 +0300

    semantic changes

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,25 +2,23 @@ import React, { useState, useEffect } from 'react'
 import '../style/Nav.css'
 
 function Nav() {
-
-    const [show, handleShow] = useState(false)
+    const [show, handleShow] = useState(false);
 
     useEffect(() => {
         const handleScroll = () => {
             if (window.scrollY > 100) {
-                handleShow(true)
+                handleShow(true);
             } else {
-                handleShow(false)
+                handleShow(false);
             }
-        }
-    
-        window.addEventListener("scroll", handleScroll)
-    
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
         return () => {
-            window.removeEventListener("scroll", handleScroll)
-        }
-    }, [])
-    
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
 
     return (
         <div className={`nav ${show && "nav_black"}`}>
@@ -29,8 +27,14 @@ function Nav() {
                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1200px-Netflix_2015_logo.svg.png"
                 alt="Netflix Logo"
             />
+            {/* Uncomment and implement the avatar icon with appropriate alt attribute */}
+            {/* <img
+                className="nav_avatar"
+                src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+                alt="Netflix User Avatar"
+            /> */}
         </div>
-    )
+    );
 }
 
 export default Nav
